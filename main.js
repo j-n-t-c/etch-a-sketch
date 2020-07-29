cb87eafb1028dc2176532671bf8e1de71ccfe200
jntc <jnthnc@protonmail.com>
2020-07-29 18:22:56 +0200

    commit updates but incomplete

--- main.js
+++ main.js
@@ -1,53 +1,149 @@
-var containerDiv = document.getElementById("container");
-var headerDiv = document.getElementById("header");
 
-const hoverOn = function() { document.querySelectorAll('.item').forEach(item => {//takes each item class div and assigns it to item
-    item.addEventListener('mouseover', event => { //when item is hovered, does function
-    item.className += " hover"; }) }) //adds hover class to apply different css    
+        /*~~~~~~~~~~first variables~~~~~~~~~~*/
+var containerDiv = document.getElementById("container1");
+var compStyles = window.getComputedStyle(containerDiv);
+var headerDiv = document.getElementById("header");
+var allItems
+var divItem
+var divArray
+var hovItem 
+var hovArray = [];
+var colorArray = ['aqua', 'brown', 'chartreuse', 'darkolivegreen', 'fuchsia', 'goldenrod',
+    'hotpink', 'indianred', 'khaki', 'limegreen', 'mediumslateblue', 'navy', 'orange', 'peru',
+     'royalblue', 'silver', 'teal', 'violet', 'wheat', 'yellow']
+var rgb = []
+var numInput = 4
+var complete = false
+var changeCount = 0
+
+
+        /*~~~~~~~~~~random color functions~~~~~~~~~~*/
+const randomColor = function(){
+for(var i = 0; i < 3; i++) {
+    rgb = colorArray[Math.floor(Math.random() * colorArray.length)];}
+for (var i = 0; i < divArray.length; i++) {
+    divArray[i].style.backgroundColor = rgb;
+}
+    return rgb;
+}
+
+const randomColor2 = function(){
+    for(var i = 0; i < 3; i++) {
+        rgb2 = colorArray[Math.floor(Math.random() * colorArray.length)];}
+        return rgb2;
     }
 
+
+const changes = function() {
+    switch(changeCount) {
+        case 1:
+        instructions.innerHTML = ('LEVEL 2: Do it two more times!');
+        removeDivs();
+        numInput = 16;
+        containerDiv.className = ('second');
+        createDivs(numInput);
+          break;
+        case 3:
+        instructions.innerHTML = ('LEVEL 3: DO IT AGAIN!');
+        removeDivs();
+        numInput = 64;
+        containerDiv.className = ('third');
+        createDivs(numInput);
+          break;
+        case 4:
+        instructions.innerHTML = ('LEVEL 4: ONE MORE TIME AND YOU UNLOCK PSYCHEDELIC MODE!');
+        removeDivs();
+        numInput = 256;
+        containerDiv.className = ('fourth');
+        createDivs(numInput);
+        break;
+        case 5:
+        instructions.innerHTML = ('LEVEL 4: ONE MORE TIME AND YOU UNLOCK PSYCHEDELIC MODE!');
+        removeDivs();
+        numInput = 1024;
+        containerDiv.className = ('fifth');
+        createDivs(numInput);
+            break;
+        /*case y:
+          // code block
+          break;
+        default:
+          // code block*/
+      }
+}
+        /*~~~~~~~~~~CREATES DIVS and assigns mouseover functions~~~~~~~~~~*/
 const createDivs = function(number) {
-    for (let i = 1; i < (number + 1); i++){
-    var newDiv = document.createElement("div"); //creates newDiv
-    newDiv.className = "item"; //gives each class name item 
-    newDiv.className += " item" + i; //gives individual item number classes
-    containerDiv.appendChild(newDiv);
-    hoverOn(); }} //puts new divs in container div x times
-    
-createDivs(256); //creates initial grid of divs onload
+    for (let i = 0; i < (number); i++){
+    var newDiv = document.createElement('div'); //creates newDiv
+    newDiv.className = 'item'; //gives each class name item 
+    newDiv.className += ' item' + i; //gives individual item number classes
+    containerDiv.appendChild(newDiv); //puts them in
+}
+    divItem = document.getElementsByClassName('item');
+    divArray = Array.from(divItem);
+    randomColor();
+    randomColor2();
+    divArray.forEach(function(item){
+        item.addEventListener('mouseenter', function() {
+            item.classList.add('hovered');
+            item.style.backgroundColor = rgb2;
+            hovItem = document.getElementsByClassName('hovered');
+            hovArray = Array.from(hovItem);
+            if(hovArray.length === numInput){
+                changeCount += 1;
+                changes();
+                divArray.forEach(function(item){
+                    item.classList.remove('hovered');
+                    randomColor();
+                    randomColor2();
+                    })
+                 }
+            })
+        })
+} 
 
-var divItem = document.getElementsByClassName("item"); /*selects all divs with class 'item'
-........................................................//returns HTML collection*/
-var divArray = Array.from(divItem); //converts HTML collection into array
+createDivs(numInput);
 
+        /*~~~~~~~~~~USER INTERFACE variables~~~~~~~~~~*/
+var resetBtn = document.getElementById('reset');
+var userInput = document.createElement("input");
+var inputBtn = document.createElement("button");
+
+        /*~~~~~~~~~~REMOVES ALL DIVS when called~~~~~~~~~~*/
 const removeDivs = function() { //loops through array of newDivs and deletes each one
     for (let i = 0; i < divArray.length; i++) {
      containerDiv.removeChild(divArray[i]);}}  
     
-    var resetBtn = document.getElementById('reset');
-    var userInput = document.createElement("input");
-    var inputBtn = document.createElement("button");
-    //RESET BUTTON TWO FUNCTIONALITY
-    resetBtn.addEventListener('click', function(){
-        location.reload();
-    });
-
-//RESET BUTTON ONE FUNCTIONALITY
-    /*resetBtn.addEventListener('click', function(){
+        /*~~~~~~~~~~RESET FUNCTION~~~~~~~~~~*/
+const reset = function() {
         removeDivs();
         userInput.setAttribute("type", "text");
         userInput.setAttribute("placeholder", "Input # of rows and columns");
         userInput.id="userInput";
-        headerDiv.appendChild(userInput);
+        headerDiv.appendChild(userInput); //puts input box on page
         inputBtn.setAttribute("type", "button");
         inputBtn.id="inputBtn";
-        inputBtn.innerHTML = 'Submit!';
+        inputBtn.innerHTML = 'Submit!'; //puts input button on page
         headerDiv.appendChild(inputBtn);
-    });*/
+}
 
+        /*~~~~~~~~~~RESET BUTTON~~~~~~~~~~*/
+    resetBtn.addEventListener('click', function(){
+        reset();
+        createDivs(numInput);
+    });
+
+        /*~~~~~~~~~~SUBMIT BUTTON~~~~~~~~~~*/
     inputBtn.addEventListener('click', function(number){
-      if (isNaN(userInput.value)) {
+      if (isNaN(userInput.value)) { //checks if input is number
         alert('You must enter a valid number!')
       } else {
-        createDivs((Number(userInput.value)));
+          reset();
+        numInput = (Number(userInput.value))
+        createDivs(numInput); //creates number of boxes based on input
     }})
+
+    /*RELOAD BUTTON IF NEEDED (change button, not reset)
+    resetBtn.addEventListener('click', function(){
+        location.reload();
+    });*/
